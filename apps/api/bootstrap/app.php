7c90ac61b7c051f12264a75127f5f5ef180942a0
moguo <t.takuma830@gmail.com>
2026-02-08 15:19:38 +0900

    修正７

--- apps/api/bootstrap/app.php
+++ apps/api/bootstrap/app.php
@@ -31,5 +31,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, \Illuminate\Http\Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => 'Unauthenticated.'], 401);
+            }
+        });
     })->create();
